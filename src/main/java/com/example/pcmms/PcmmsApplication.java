9786038c8fcbe040784c085f21c36c7dea3eee15
Mqondisi <thefirstterry@gmail.com>
2023-07-28 09:46:48 +0200

    initial commit

---
+++ src/main/java/com/example/pcmms/PcmmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pcmms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcmmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcmmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pcmms/PcmmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pcmms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcmmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
